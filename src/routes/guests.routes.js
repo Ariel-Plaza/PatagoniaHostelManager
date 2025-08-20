0d18ebe100a088dd2bf7d9256b31be75a1377564
Ariel Plaza <arielplazasalinas@gmail.com>
2025-08-20 16:33:22 -0300

    update routes views

--- src/routes/guests.routes.js
+++ src/routes/guests.routes.js
@@ -1,5 +1,4 @@
 import { Router } from "express";
-
 import { createGuest, readAllGuest }  from "../controllers/guests.controllers.js";
 
 const router = Router();
@@ -7,6 +6,5 @@ const router = Router();
 //ruta agregar nuevo huesped
 router.post('/', readAllGuest)
 router.post('/create', createGuest)
-
 
 export default router;
